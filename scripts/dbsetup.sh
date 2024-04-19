8391c766ebb17fe54a1d3580292b1401b19d086f
Macen Bird <50558070+macenb@users.noreply.github.com>
2024-04-19 14:50:50 -0600

    Update dbsetup.sh

--- scripts/dbsetup.sh
+++ scripts/dbsetup.sh
@@ -5,4 +5,6 @@ DB_NAME=""
 # make new user, grant permissions, set password
 # drop permissions of postgres user
 
+sudo -u postgres createuser -s blueteam
+createdb db
 psql -U $USERNAME -d $DB_NAME -f ncaedb.sql
